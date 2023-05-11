85ed9830101f5214f089f0594e71c20a7a4e58e5
aleksa <zabumaphu@gmail.com>
2023-05-11 00:05:00 -0400

    First commit. More hacking to come!!!

---
+++ ch2/tcpServer.py
@@ -0,0 +1,32 @@
+"""
+Author: Aleksa Zatezalo
+Date: May 10th 2023
+Description: A Basic TCP Server in Python
+"""
+
+import socket
+import threading
+
+IP = '0.0.0.0'
+PORT = 9998
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, PORT))
+    server.listen(5)
+    print(f'[*] Listening on {IP}:{PORT}')
+
+    while True:
+        client, address = server.accept()
+        print(f'[*] Accepted connection from {address[0]}')
+        client_handler = threading.Thread(target=handel_client, args=(client,))
+        client_handler.start
+
+def handel_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Recived: {request.decode("utf-8")}')
+        sock.send(b'ACK')
+
+if __name__ == '__main__':
+    main()
