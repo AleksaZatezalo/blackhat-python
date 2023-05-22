8d98758214d8fa279861c3e1eb65dafcef0be7bc
aleksa <zabumaphu@gmail.com>
2023-05-21 22:25:02 -0400

    Working one packet sniffer.

--- ch3/sniffer.py
+++ ch3/sniffer.py
@@ -1,24 +1,36 @@
 """
 Author: Aleksa Zatezalo
-Date: May 15th 2023
-Description: The begginings of a sniffer in python.
+Date: May 21th 2023
+Description: A sniffer written in python.
 """
 
-from ctypes import *
-import struct
 import socket
 import os
 
+# host to listen on
+HOST = '192.168.0.111'
 
 def main():
-	# create raw socket, bin to public interface
+    # create a raw socket, bin to public interface
 	if os.name == 'nt':
 		socket_protocol = socket.IPPROTO_IP
 	else:
 		socket_protocol = socket.IPPROTO_ICMP
+
+	sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+	sniffer.bind((HOST,0))
+	# include the IP header in the capture
+	sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 	
-	sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocal)
-	sniffer.bind(HOST,0)
-	# include the IP header in capture
-	sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL,1)
+	if os.name == 'nt':
+		sniffer.iotcl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+	# read one packet
+	print(sniffer.recvfrom(65565))
+
+	# if we're on Windows, turn off promiscous mode
+	if os.name == 'nt':
+		sniffer.iotcl(socket.SIO_RCVALL, socket.RCVALL_OFF)
 	
+if __name__ == '__main__':
+	main()
