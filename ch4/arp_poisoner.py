f1b03ba76ada8decfac2b5762bbe616cde795b47
aleksa <zabumaphu@gmail.com>
2023-05-22 21:39:06 -0400

    Poision Func Finished.

--- ch4/arp_poisoner.py
+++ ch4/arp_poisoner.py
@@ -28,8 +28,17 @@ class Arper:
         conf.iface = interface
         conf.verb = 0
 
+        print(f'Initialized {interface}:')
+        print(f'Gateway ({gateway} is set at {self.gatewaymac}.)')
+        print(f'Victim ({victim}) is at {self.victim}.')
+        print('-' * 30)
+
     def run(self):
-        pass
+        self.poison_thread = Process(target=self.poison)
+        self.poison_thread.start()
+
+        self.sniff_thread = Process(target=self.sniff)
+        self.sniff_thread.start()
 
     def poison(self):
         pass
