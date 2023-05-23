04d22543185e2bd4a2f0726b40e012733dca6063
aleksa <zabumaphu@gmail.com>
2023-05-22 22:08:30 -0400

    Pcapper.py skeleton finished.

--- ch4/pcapper.py
+++ ch4/pcapper.py
@@ -5,3 +5,35 @@ Description: PCAP coded in python.
 """
 
 from scapy.all import TCP, rdpap
+import collections
+import os
+import re
+import sys
+import zlib
+
+OUTDIR = '/root/Desktop/pictures'
+PCAPS = '/root/Downloads'
+
+Response = collections.namedtupple('Response', ['header', 'payload'])
+
+def get_header(payload):
+    pass
+
+def exctract_content(Response, content_name='image'):
+    pass
+
+class Recapper:
+    def __init__(self):
+        pass
+
+    def get_responses(self):
+        pass
+
+    def write(self, content_name):
+        pass
+
+if __name__ == '__main__':
+    pfile = os.path.join(PCAPS, 'pcap.pcap')
+    recapper = Recapper(pfile)
+    recapper.get_responses()
+    recapper.write('image')
